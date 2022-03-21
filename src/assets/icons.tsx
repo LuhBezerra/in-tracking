b6f6183a151adee8e7ea39f3dbd5ce9fe9573aea
LuhBezerra <lucilenebezerra.of@gmail.com>
2022-03-21 13:41:02 -0300

    style: Add assets

---
+++ src/assets/icons.tsx
@@ -0,0 +1,6 @@
+export {ReactComponent as LogoIcon} from './logo.svg';
+export {ReactComponent as ClearIcon} from './clear.svg';
+export {ReactComponent as AlertCircleIcon} from './circle-alert.svg';
+export {ReactComponent as DropdownDownIcon} from './dropdown.svg';
+export {ReactComponent as CheckIcon} from './check.svg';
+export {ReactComponent as PlusIcon} from './plus.svg';
